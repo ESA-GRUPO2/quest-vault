7b62a45823686a7587d9239e05870d887e27e7e4
João Coelho <84146833+joaortcoelho@users.noreply.github.com>
2024-01-18 16:09:48 +0000

    add bootstrap to angular.json and replaced header component with nav-menu

--- questvault/questvault.client/src/app/app.module.ts
+++ questvault/questvault.client/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
--- questvault/questvault.client/src/app/nav-menu/nav-menu.component.spec.ts
+++ questvault/questvault.client/src/app/nav-menu/nav-menu.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { NavMenuComponent } from './nav-menu.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('NavMenuComponent', () => {
+  let component: NavMenuComponent;
+  let fixture: ComponentFixture<NavMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      declarations: [NavMenuComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(NavMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
