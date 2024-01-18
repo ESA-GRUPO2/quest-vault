f9a8a813cdbcb7f3848de89cd0e6ffd27cc5ace2
João Coelho <84146833+joaortcoelho@users.noreply.github.com>
2024-01-18 23:22:45 +0000

    nav-menu draft

--- questvault/questvault.client/src/app/nav-menu/nav-menu.component.ts
+++ questvault/questvault.client/src/app/nav-menu/nav-menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css'
 })
 export class NavMenuComponent {
-
+  
 }
