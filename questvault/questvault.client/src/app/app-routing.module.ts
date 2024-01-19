d2794a4c129d4fefdcfb091bb1fc77fc5bf852f4
João Coelho <84146833+joaortcoelho@users.noreply.github.com>
2024-01-19 15:26:49 +0000

    create signup-page component

--- questvault/questvault.client/src/app/app-routing.module.ts
+++ questvault/questvault.client/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component'
 
 const routes: Routes = [
-  { path: '', component: LoginPageComponent, pathMatch:'full'}];
+  { path: '/login', component: LoginPageComponent, pathMatch: 'full' },
+  { path: '/signup', component: SignupPageComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
