e2b6c4350f9280eb3f7260fc59f724ddf7fc0bdc
João Coelho <84146833+joaortcoelho@users.noreply.github.com>
2024-03-10 23:26:30 +0000

    first draft

--- questvault/questvault/Controllers/HomeController.cs
+++ questvault/questvault/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace questvault.Controllers
             return View();
         }
 
+        public IActionResult GameDetails() 
+        { 
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
