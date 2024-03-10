2a3a9fa90ee96b87b181c809e10a5d78d76c7455
Luis Rocha <lmcr666@gmail.com>
2024-03-10 18:57:36 +0000

    azure all day

--- questvault/questvault/Migrations/20240310174026_init.cs
+++ questvault/questvault/Migrations/20240310174026_init.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace questvault.Migrations
 {
     /// <inheritdoc />
-    public partial class Init : Migration
+    public partial class init : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
