79d79cf43d7f8ab3321f5f50cb4264c900482ba9
Nuno Martinho <nuno.m.martinho@gmail.com>
2024-03-11 17:49:51 +0000

    Created necessary models for games and genres to work properly in database, first population done

---
+++ questvault/questvault/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace questvault.Models
+
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyID { get; set; }
+        public string CompanyName { get; set; }
+
+        // Relacionamento Muitos-Para-Muitos com Games
+        public virtual List<Games>? Games { get; set; }
+    }
+}
