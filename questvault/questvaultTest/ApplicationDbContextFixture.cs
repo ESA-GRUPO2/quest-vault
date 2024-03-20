a7b2cacf92c4b221c343c2668a663dbe9fd63bc2
Nuno Martinho <nuno.m.martinho@gmail.com>
2024-03-20 19:20:59 +0000

    testeasdjlhbashjdgasjhdgasjh

---
+++ questvault/questvaultTest/ApplicationDbContextFixture.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using questvault.Data;
+using questvault.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questvaultTest
+{
+    public class ApplicationDbContextFixture : IDisposable
+    {
+        public ApplicationDbContext DbContext { get; private set; }
+        private IServiceIGDB igdbService;
+        public ApplicationDbContextFixture()
+        {
+            igdbService = new IGDBService("uzhx4rrftyohllg1mrpy3ajo7090q5", "7rvcth933kxra92ddery5qn3jxwap7");
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+            DbContext = new ApplicationDbContext(options, igdbService);
+
+            DbContext.Database.EnsureCreated();
+        }
+
+
+
+
+        public void Dispose() => DbContext.Dispose();
+    }
+}
---
+++ questvault/questvaultTest/GamesControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using questvault.Controllers;
+using questvault.Data;
+using questvault.Models;
+using questvault.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questvaultTest
+{
+    public class GamesControllerTests
+    {
+       
+    }
+}
