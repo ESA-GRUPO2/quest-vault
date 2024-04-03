f4bf7e1f02fe56470b9ecd2f15e01de043ab2357
Batista-10 <tiagobatista6.gpsi@gmail.com>
2024-04-03 22:53:36 +0100

    Consultar numero de contas, e adicionei clearence para aceder a pagina

---
+++ questvault/questvault/Controllers/StatisticsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using questvault.Data;
+using questvault.Models;
+
+namespace questvault.Controllers
+{
+    public class StatisticsController(ApplicationDbContext context, SignInManager<User> signInManager) : Controller
+    {
+        /// <summary>
+        ///     Makes a list with all the user's friends
+        /// </summary>
+        [ActionName("StatisticsPage")]
+        public async Task<IActionResult> StatisticsPageAsync()
+        {
+            var user = await signInManager.UserManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            var registeredAccounts = await context.Users.ToListAsync();
+
+            return View("StatisticsPage", registeredAccounts.Count);
+        }
+    }
+}
---
+++ questvault/questvault/Models/Statistics.cs
@@ -0,0 +1,10 @@
+﻿namespace questvault.Models
+{
+    public class Statistics
+    {
+        public int registeredUsersCount { get; set; }
+        //public int siteAccessCount { get; set; }
+        //public List<> dailyLoggins { get; set; }
+
+    }
+}
