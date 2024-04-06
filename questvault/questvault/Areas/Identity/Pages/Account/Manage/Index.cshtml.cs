0ed52c7e40c7d71e1ceb6846b58b0a43be8e7493
samuelfr19 <samuelferreira2001@gmail.com>
2024-04-06 17:51:56 +0100

    commit

--- questvault/questvault/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ questvault/questvault/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -5,6 +5,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using questvault.Controllers;
 using questvault.Data;
 using questvault.Models;
