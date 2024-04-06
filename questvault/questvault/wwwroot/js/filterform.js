6264664dd5de898b8cbcf4d04370b1a096ea1797
João Coelho <84146833+joaortcoelho@users.noreply.github.com>
2024-04-06 16:10:13 +0100

    bootstrap-select more tweaks

--- questvault/questvault/wwwroot/js/filterform.js
+++ questvault/questvault/wwwroot/js/filterform.js
@@ -1,12 +1,12 @@
 function clearForm() {
-    document.getElementById("filterForm").reset();
+  document.getElementById("filterForm").reset();
 }
 
 // selectpicker for select in forms
 
 $('.select-results').selectpicker({
-    dropupAuto: false,
-    liveSearch: true,
-    size: 7,
-    style: 'btn-dark',
+  dropupAuto: false,
+  liveSearch: true,
+  size: 7,
+  style: 'btn-dark; background-color: #133E7C;'
 });
